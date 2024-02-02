a6473f904a065dddb6ed9aeb46be0755b6edb3ac
wilmitch <mwangimwilfred@gmail.com>
2024-02-02 12:46:01 +0300

    added correction to task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-#This regex expression accepts one argument and passes it to a regular expression matching method
-
-puts ARGV[0].scan(/school/).join
+# Simply matching Holberton
+puts ARGV[0].scan(/School/).join
