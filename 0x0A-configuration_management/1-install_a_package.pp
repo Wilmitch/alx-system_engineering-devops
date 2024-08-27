004cad406870bd6ff01740945df037d1a2836c4d
wilmitch <mwangimwilfred@gmail.com>
2024-08-27 10:41:40 +0300

    install a flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,11 @@
 #install flask verion 2.1.0 as well as werkzeug
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
 package { 'werkzeug':
-  ensure   => 'installed',
+  ensure => '2.1.1',
   provider => 'pip3',
-  require  => Package['flask'],
 }
 
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
+}
